822f0f7dc3e5e8f83d9744e707c133d907c15e4b
Damir <damirius@gmail.com>
2016-02-22 06:44:42 +0100

    Added add event submit

--- client/app/eventadd/eventadd.js
+++ client/app/eventadd/eventadd.js
@@ -16,6 +16,29 @@
     vm.who_pays = 1;
     vm.whose_place = 1;
     vm.country = "";
+    vm.countries = [];
+    vm.submit = submit;
+
+    function submit()
+    {
+      var params = {
+        city: vm.city,
+        country: vm.country,
+        whose_place: vm.whose_place,
+        who_pays: vm.who_pays,
+        cost_estimate: vm.cost_estimate,
+        tags: vm.tags,
+        when: vm.when,
+        description: vm.description,
+        meal_name: vm.meal_name
+      };
+      AuthRestangular.all('events').post(params).then(function(event){
+          console.log(event);
+        });
+    }
+    AuthRestangular.all('countries').getList().then(function(countries){
+      vm.countries = countries;
+    });
     AuthRestangular.all('events').getList().then(function (events) {
       vm.events = events;
     });
